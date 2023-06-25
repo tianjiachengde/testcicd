d9b2b660ee5aa2f424609e721a4443b372d675e4
田家诚 <729765717@qq.com>
2023-06-25 15:54:23 +0800

    feat: update content

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
-    <h1>测试自动打包发布</h1>
+    <h1>测试自动打包发布{{new Date() }}</h1>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
